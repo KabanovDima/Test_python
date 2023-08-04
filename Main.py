e637c56e4479717dfdae0116cb1ebcba8ea36271
Dmitry Kabanov <kabanov_d@bk.ru>
2023-08-04 21:56:40 +0600

    Did task

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 
@@ -19,12 +20,68 @@ def showData(data):
     else:
         print("Заметки:")
         for note in data:
-            print(f"ID: {note['id']}, Заголовок: {note['title']}, Дата/время создания: {note['time']}")
+            print(f"ID: {note['id']}; Заголовок: {note['title']}; Тело заметки: {note['body']}; Дата/время создания: {note['timestamp']}")
 
-def main():
-    data = loadData()
-    showData(data)
+def addData(data, title, body):
+    new_data = {
+        "id": len(data) + 1,
+        "title": title,
+        "body": body,
+        "timestamp": datetime.datetime.now().isoformat()
+    }
+    data.append(new_data)
+    print("Заметка добавлена")
 
-main()
+def editData(data, note_id, title, body):
+    """Редактирование существующей заметки"""
+    for note in data:
+        if note["id"] == note_id:
+            note["title"] = title
+            note["body"] = body
+            note["timestamp"] = datetime.datetime.now().isoformat()
+            print("Заметка успешно отредактирована!")
+            return
+    print("Заметка с таким ID не найдена.")
+
+def deleteData(data, note_id):
+    """Удаление заметки по ID"""
+    for note in data:
+        if note["id"] == note_id:
+            data.remove(note)
+            print("Заметка успешно удалена!")
+            return
+    print("Заметка с таким ID не найдена.")
+
+
 data = loadData()
-showData(data)
+while True:
+    print("\nВыберите команду:")
+    print("1. Просмотреть все заметки")
+    print("2. Добавить новую заметку")
+    print("3. Редактировать заметку")
+    print("4. Удалить заметку")
+    print("5. Выйти")
+
+    choise = input("Введите номер команды: ")
+    if choise == "1":
+        showData(data)
+    elif choise == "2":
+        title = input("Введите заголовок заметки: ")
+        body = input("Введите текст заметки: ")
+        addData(data, title, body)
+    elif choise == "3":
+        note_id = int(input("Введите ID заметки, которую хотите отредактировать: "))
+        title = input("Введите заголовок заметки: ")
+        body = input("Введите текст заметки: ")
+        editData(data,note_id, title, body)
+    elif choise == "4":
+        note_id = int(input("Введите ID заметки, которую хотите удалить: "))
+        deleteData(data, note_id)
+    elif choise == "5":
+        saveData(data)
+        print("Пока")
+        break
+    else:
+        print("Некорректный ввод. Попробуйте еще раз")
+
+
